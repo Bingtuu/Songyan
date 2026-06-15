dc9d87760bb0024fbcfa3d363f0b1b49af05bd78
Songyan <songyan@example.com>
2026-06-15 19:17:20 +0800

    chore: remove _tmp_git_fixup directory

--- _tmp_git_fixup/_doit.py
+++
@@ -1,4 +0,0 @@
-import pathlib
-fp = pathlib.Path(r" c:\Vibe Project\Songyan\src\songyan\workflows\_nodes.py\)
-c = fp.read_text(encoding=" utf-8\)
-
--- _tmp_git_fixup/test_quotes2.py
+++
@@ -1,3 +0,0 @@
-text = 'hello "world"'
-open(r'c:\Vibe Project\Songyan\test_quotes2.txt', 'w').write(text)
-print('written')
